cae14269843ab9fd736a1b34d4148c7f4fce338d
7rans <transfire@gmail.com>
2012-02-02 22:56:10 -0500

    Fix ignore callers to match tapunit.

--- lib/tapunit/ignore_callers.rb
+++ lib/tapunit/ignore_callers.rb
@@ -1,4 +1,4 @@
-ignore = /(lib|bin)#{Regexp.escape(File::SEPARATOR)}minitap/
+ignore = /(lib|bin)#{Regexp.escape(File::SEPARATOR)}tapunit/
 
 $RUBY_IGNORE_CALLERS ||= []
 $RUBY_IGNORE_CALLERS << ignore
